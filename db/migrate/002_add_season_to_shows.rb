02c3e33e75586d072d43aab80c383c85a7e09358
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-22 21:08:03 +0000

    automatically backed up by learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,5 +1,5 @@
-class AddSeasonToShow < ActiveRecord::Migration[4.2]
-
-  def change
-    add_column :shows, :season, :string
-end
+# class AddSeasonToShow < ActiveRecord::Migration[4.2]
+#
+#   def change
+#     add_column :shows, :season, :string
+# end
