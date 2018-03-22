a15d5ca355415ad031667c5def331be6d0a01f65
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-22 20:57:00 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,12 +1,12 @@
 class CreateShows < ActiveRecord::Migration
 
-#   def change
-#     create_table :shows do |s|
-#       s.string :name
-#       s.string :network
-#       s.s.integer string :day
-#       s.integer :rating
-#   end
-# end
+  def change
+    create_table :shows do |s|
+      s.string :name
+      s.string :network
+      s.s.integer string :day
+      s.integer :rating
+  end
+end
 
 end
